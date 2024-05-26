46c366ce5c0ca7351fa7b9bc0d519f4742e42150
Haitem Bahri <haitem_bahri@yahoo.com>
2024-05-26 01:29:50 +0100

    updating and adding comparison methods to MonetaryResultItem

--- src/UKTakeHomePayCalculator.Core/Models/MonetaryResultItem.cs
+++ src/UKTakeHomePayCalculator.Core/Models/MonetaryResultItem.cs
@@ -2,7 +2,7 @@
 
 namespace UKTakeHomePayCalculator.Core.Models;
 
-public class MonetaryResultItem
+public class MonetaryResultItem : IComparable<MonetaryResultItem>
 {
     public MonetaryRuleItem Rule { get; }
     public decimal RulePercentage { get => Rule.Percentage; }
@@ -21,7 +21,7 @@ public class MonetaryResultItem
 
     public override string ToString()
     {
-        return $"Rule = {Rule}, Result = {Result}";
+        return $"{Rule} = {Result}";
     }
     
     public static bool operator==(MonetaryResultItem value1, MonetaryResultItem value2)
@@ -34,6 +34,49 @@ public class MonetaryResultItem
     {
         return !(value1 == value2);
     }
+    
+    public static bool operator >(MonetaryResultItem value1, MonetaryResultItem value2)
+    {
+        if (value1.Rule > value2.Rule)
+            return true;
+
+        return false;
+    }
+    
+    public static bool operator >=(MonetaryResultItem value1, MonetaryResultItem value2)
+    {
+        if (value1.Rule >= value2.Rule)
+            return true;
+
+        return false;    
+    }
+    
+    public static bool operator <(MonetaryResultItem value1, MonetaryResultItem value2)
+    {
+        if (value1.Rule < value2.Rule)
+            return true;
+
+        return false;    
+    }
+    
+    public static bool operator <=(MonetaryResultItem value1, MonetaryResultItem value2)
+    {
+        if (value1.Rule <= value2.Rule)
+            return true;
+
+        return false;
+    }
+
+    public int CompareTo(MonetaryResultItem? other)
+    {
+        if (this < other)
+            return -1;
+
+        if (this == other)
+            return 0;
+
+        return 1;
+    }
 
     public override bool Equals(object? obj)
     {
@@ -44,5 +87,10 @@ public class MonetaryResultItem
             return false;
         
         return this == (MonetaryResultItem)obj;
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
     }
 }
--- test/UKTakeHomePayCalculator.Tests/ModelsTests/MonetaryResultItemTests.cs
+++ test/UKTakeHomePayCalculator.Tests/ModelsTests/MonetaryResultItemTests.cs
@@ -8,9 +8,9 @@ public class MonetaryResultItemTests
 {
     public static TheoryData<MonetaryRuleItem, MonetaryValue, string> ShouldReturnCorrectStringTheoryData => new()
     {
-        {new MonetaryRuleItem(MonetaryValue.Zero, 3500m.Year(), 0.08m), 120m.Week(), "Rule = [From 0.00/Week - To 3,500.00/Year] %8.00, Result = 120.00/Week"},
-        {new MonetaryRuleItem(1200m.Year(), 3500m.Year(), 0.12m), 10500m.Year(), "Rule = [From 1,200.00/Year - To 3,500.00/Year] %12.00, Result = 10,500.00/Year"},
-        {new MonetaryRuleItem(450000m.Year(), MonetaryValue.PositiveInfinity, 0.02m), 2890m.Month(), "Rule = [From 450,000.00/Year - To +Infinity] %2.00, Result = 2,890.00/Month"},
+        {new MonetaryRuleItem(MonetaryValue.Zero, 0.08m), 120m.Week(), "From 0.00 @ %8.00 = 120.00/Week"},
+        {new MonetaryRuleItem(1200m.Year(), 0.12m), 10500m.Year(), "From 1,200.00/Year @ %12.00 = 10,500.00/Year"},
+        {new MonetaryRuleItem(450000m.Year(), 0.02m), 2890m.Month(), "From 450,000.00/Year @ %2.00 = 2,890.00/Month"},
     };
 
     [Theory]
